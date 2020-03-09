8751ee94304dde42aca232274dc58174cb895e5a
enes <enescihancinar@gmail.com>
2020-03-08 23:55:02 -0700

    interceptor added for auth

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from 'axios'
+import { AsyncStorage } from 'react-native'
 
-export default axios.create({
+const instance = axios.create({
     baseURL: 'https://xrider-api.herokuapp.com'
 })
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token')
+
+        if (token)
+            config.headers.Authorizationn = `Bearer ${token}`
+
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    }
+)
+
+export default instance
