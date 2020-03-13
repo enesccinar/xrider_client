9855d5f13d694d684c53e090e1456232f0c896fe
enes <enescihancinar@gmail.com>
2020-03-12 23:48:41 -0700

    trail record list screen is ready

--- src/screens/TrackRecordListScreen.js
+++ src/screens/TrackRecordListScreen.js
@@ -1,11 +1,32 @@
-import React from 'react'
-import { Text, StyleSheet, Image } from 'react-native'
+import React, { useContext } from 'react'
+import { Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native'
+import { NavigationEvents, SafeAreaView } from 'react-navigation'
+import { ListItem } from 'react-native-elements'
+import { Context as TrackRecordContext } from '../context/TrackRecordContext'
+import Spacer from '../components/Spacer'
 
 const TrackRecordListScreen = ({ navigation }) => {
+    const { state, fetchTrackRecords } = useContext(TrackRecordContext)
+
     return (
-        <>
-            <Text>Text</Text>
-        </>
+        <SafeAreaView>
+            <NavigationEvents onWillFocus={fetchTrackRecords} />
+            <Spacer>
+                <Text style={styles.header}>Trail Records</Text>
+            </Spacer>
+            <FlatList
+                data={state}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => {
+
+                        }}>
+                            <ListItem chevron title={item.name} />
+                        </TouchableOpacity>
+                    )
+                }} />
+        </SafeAreaView>
     )
 }
 
@@ -14,13 +35,20 @@ TrackRecordListScreen.navigationOptions = {
     tabBarOptions: {
         activeTintColor: '#6540F5',
     },
-    tabBarIcon: ({focused}) => (
+    tabBarIcon: ({ focused }) => (
         focused
-        ? <Image source={require('../../assets/tracks.png')} />
-        : <Image source={require('../../assets/tracks.png')} />
+            ? <Image source={require('../../assets/tracks.png')} />
+            : <Image source={require('../../assets/tracks.png')} />
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        fontWeight: 'bold',
+        fontSize: 32,
+        marginBottom: 20,
+        marginLeft: 24
+    }
+})
 
 export default TrackRecordListScreen
